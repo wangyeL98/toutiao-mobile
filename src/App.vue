c89dba100db29b340cb9717fb66ef3ec7908f84c
wangye <1820908535@qq.com>
2022-10-13 17:45:44 +0800

    引入vant

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,20 @@
 <template>
   <div>
     <h1>黑马头条</h1>
+    <i class="toutiao toutiao-yuedu"></i>
+    <div>
+      <van-button type="primary">主要按钮</van-button>
+      <van-button type="info">信息按钮</van-button>
+      <van-button type="default">默认按钮</van-button>
+      <van-button type="warning">警告按钮</van-button>
+      <van-button type="danger">危险按钮</van-button>
+    </div>
   </div>
-
 </template>
 
 <script>
-export default {
-
-}
+export default {}
 </script>
 
 <style>
-
 </style>
